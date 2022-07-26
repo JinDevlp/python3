6318226c7e0647f43f84e6e371599cf4d684f74f
Jin Choi <91814116+JinDevlp@users.noreply.github.com>
2022-07-26 16:11:43 -0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+""" Primitive Data Types"""
+
+name = "Jin"  # Variable name has a string value of "Jin"
+
+age = 27  # Variable age has a integer value of 27
+
+cash = 200.22  # Variable cash has a float value of 200.22
+
+employed = False  # Variable employed has a boolean value of False
+
+""" Composite Data Types"""
+countries_lived = ["Italy", "South Korea",
+                   "USA"]  # List/ Ordered sequence/ mutable
+
+# Dictionary/ Unordered collection of key-value pairs
+jin_info = {"name": "Jin", "age": 27, "cash": 200.22, "retired": False}
+
+# Tuple/ Similar to a list, but immutable
+my_tuple = ("python", "django", "sql")
+
+# Set/ Unordered collection of immutable, unique values
+my_sef = {"flask", "docker", "aws"}
